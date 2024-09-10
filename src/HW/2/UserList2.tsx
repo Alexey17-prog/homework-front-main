3d8b8d1dd7230a1e0c81880397be261dc32ffe8d
Alexey17 <axelymasysadm@gmail.com>
2024-09-10 20:55:07 +0300

    first commit

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -1,24 +1,23 @@
-import { CurrentUser } from './CurrentUser';
-import {UsersObjectType} from "./HW2";
+import {CurrentUser} from './CurrentUser';
+import {UsersObjectType} from './HW2';
 
-// Используйте уже определенные типы для пропсов
-export type UserList2PropsType = {
-	users: UsersObjectType; // Теперь типизировано
-	filterUsers: () => void; // Теперь типизировано
+type UserList2PropsType = {
+	users: any // пропиши типизацию
+	filterUsers: any // пропиши типизацию
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
 	return (
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
-			{/* Исправьте вызов filterUsers, чтобы он был функцией */}
-			<button id={'hw02-filter-button'} onClick={props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
-			<ul>
-				{/* Используйте компонент CurrentUser и передайте ему пользователя как проп */}
-				{props.users.myFriends.map((user) => (
-					<CurrentUser key={user.id} user={user} />
-				))}
-			</ul>
+
+			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
+
+			{/*<ul>*/}
+			{/*  {props.users.myFriends.map((user) => (*/}
+			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
+			{/*  ))}*/}
+			{/*</ul>*/}
 		</div>
 	);
 };
